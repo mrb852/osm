167278e9b79892648a7faef64e839c9bb92acccb
Troels Ynddal <troelsynddal@gmail.com>
2015-02-13 14:40:12 +0100

    Added comments and changed the reserved sapce from 8 to 64 elements

--- G1/heap.c
+++ G1/heap.c
@@ -5,83 +5,84 @@
 
 void heapify(heap_t* heap, size_t i, size_t n) {
 
-  long l = 2 * i + 1;
-  long r = 2 * i + 2;
-  long max = i;
-  node_t *arr = heap->root;
+  long unsigned l = 2 * i + 1; // long unsigned, since we can't have negative
+  long unsigned r = 2 * i + 2; // heaps.
+  long unsigned max = i; // max is always positive or zero
+  node_t *arr = heap->root; // pointer to root
 
+  // Find max
   if (l < n && arr[l].priority > arr[max].priority) { max = l; }
   if (r < n && arr[r].priority > arr[max].priority) { max = r; }
 
   if (max != i) {
-    node_t tmp = arr[i];
-    arr[i] = arr[max];
-    arr[max] = tmp;
-    heapify(heap, max, n);
+    node_t tmp = arr[i]; // Backup element i
+    arr[i] = arr[max]; // Overwrite [i] with [max]
+    arr[max] = tmp; // [i] is new [max]
+    heapify(heap, max, n); // Run heapify recurrsion
   }
 }
 
 void build_heap(heap_t* heap) {
-  long n = heap_size(heap);
-  long i = n / 2;
+  long unsigned n = heap_size(heap); // We expect size to be non-negative
+  long unsigned i = n / 2;
 
-  while(i > 0) {
+  while(i) { // while is not zero (same as false)
     heapify(heap, --i, n);
   }
 }
 
 void heap_initialize(heap_t* heap) {
-  heap->alloc_size = 8 * sizeof(node_t);
+  heap->alloc_size = 64 * sizeof(node_t);
   heap->size = 0;
   heap->root = malloc(heap->alloc_size);
 }
 
 size_t heap_size(heap_t* heap) {
-  return heap->size / sizeof(node_t);
+  return heap->size / sizeof(node_t); // logical size is given by dividing
+                                      // bytes-size and size of a single element
 }
 
 void* heap_top(heap_t* heap) {
-  if (heap->size <= 0) {
+  if (heap->size <= 0) { // Make sure the heap is not empty
     return NULL;
   }
-  
-  return heap->root->value;
+
+  return heap->root->value; // return top element
 }
 
 void heap_insert(heap_t* heap, void* obj, int p) {
-  assert(heap->root != NULL);
-  heap->size += sizeof(node_t);
+  assert(heap->root != NULL); // Make sure the heap is not empty
+  heap->size += sizeof(node_t); // Increment size of heap
 
-  if (heap->size > heap->alloc_size) {
-    heap->alloc_size *= 2;
+  if (heap->size > heap->alloc_size) { // If all space is used:
+    heap->alloc_size *= 2;             // Reserve double the space
     heap->root = realloc(heap->root, heap->alloc_size);
   }
   node_t node = { .value = obj, .priority = p };
 
   heap->root[heap_size(heap) - 1] = node;
   build_heap(heap);
-
 }
 
 void swap(node_t arr[], int i, int j) {
-  node_t tmp = arr[i];
-  arr[j] = arr[i];
-  arr[i] = tmp;
+  node_t tmp = arr[i]; // Save temporary
+  arr[j] = arr[i]; // overwrite [j] with [i]
+  arr[i] = tmp; // overwrite [i] with tmp
 }
 
 void* heap_pop(heap_t* heap) {
-  assert(heap->size > 0);
+  assert(heap->size > 0); // Can't pop an empty heap
   node_t *root = heap->root;
   void *val = root->value;
 
-  for (int i = 1; i < heap_size(heap); ++i) {
+  for (unsigned i = 1; i < heap_size(heap); ++i) {
     swap(heap->root, i, i - 1);
   }
 
-  heap->size -= sizeof(node_t);
+  heap->size -= sizeof(node_t); // decrement size
 
-  if (heap->size <= heap->alloc_size / 2) {
-    heap->alloc_size /= 2;
+  if (heap->size <= heap->alloc_size / 2) { // If heap size is all than half the
+    heap->alloc_size /= 2;                  // allocated size: Free up space.
     heap->root = realloc(heap->root, heap->alloc_size);
   }
 
@@ -91,11 +92,10 @@ void* heap_pop(heap_t* heap) {
 }
 
 void heap_clear(heap_t *heap) {
-  for (long i = 0; i < heap_size(heap); ++i) {
+  for (long unsigned i = 0; i < heap_size(heap); ++i) {
     node_t node = heap->root[i];
     if (node.value != NULL) {
       free(node.value);
-      node.priority = 0;
     }
   }
   free(heap->root);
--- G1/main.c
+++ G1/main.c
@@ -22,7 +22,7 @@ void assertTrue(char *message, int test) {
   printf("Performing %s: %s\n", message, test ? "true" : "false");
 }
 
-void testTop() {
+void testTop() { // Test that the top element is the largest element
   heap_t heap;
   heap_initialize(&heap);
   heap_insert(&heap, createObject("Tester", 10), 10);
@@ -30,20 +30,27 @@ void testTop() {
   heap_insert(&heap, createObject("This will be the top", 1337), 1337);
   heap_insert(&heap, createObject("Tester", 210), 210);
   heap_insert(&heap, createObject("Tester", 110), 110);
-  char *top = heap_top(&heap);
+  char *top = heap_top(&heap); // This should be 1337
   assertTrue("testTop", strcmp(top, "This will be the top: 1337") == 0);
   heap_clear(&heap);
 }
 
-void testTopAfterPop() {
+void testTopAfterPop() { // check that top is the highest priority after pop
   heap_t heap;
   heap_initialize(&heap);
   heap_insert(&heap, createObject("Tester", 10), 10);
   heap_insert(&heap, createObject("Tester", 11), 11);
+  heap_insert(&heap, createObject("Tester", 12), 12);
+  heap_insert(&heap, createObject("Tester", 13), 13);
+  heap_insert(&heap, createObject("Tester", 14), 14);
+  heap_insert(&heap, createObject("Tester", 15), 15);
   heap_insert(&heap, createObject("This will be the top", 1337), 1337);
+  heap_insert(&heap, createObject("Tester", 16), 16);
+  heap_insert(&heap, createObject("Tester", 17), 17);
   heap_insert(&heap, createObject("Tester", 2100), 2100);
   heap_insert(&heap, createObject("Tester", 110), 110);
-  heap_pop(&heap);
+
+  heap_pop(&heap); // pop 2100, making 1337 as top
   char *top = heap_top(&heap);
   assertTrue("testTopAfterPop", strcmp(top, "This will be the top: 1337") == 0);
   heap_clear(&heap);
@@ -52,8 +59,8 @@ void testTopAfterPop() {
 void testHeapClear() {
   heap_t heap;
   heap_initialize(&heap);
-  for (int i = 0; i < 2000; ++i) {
-    heap_insert(&heap, createObject("hejsa", i), i);
+  for (int i = 0; i < 2000; ++i) { // Insert 2000 elements and clear them after
+    heap_insert(&heap, createObject("Tester", i), i);
   }
   heap_clear(&heap);
   assertTrue("testHeapClear", heap_top(&heap) == NULL && heap_size(&heap) == 0);
@@ -63,11 +70,11 @@ void testHeapSize() {
   heap_t heap;
   heap_initialize(&heap);
   for (int i = 0; i < 16; ++i) {
-    heap_insert(&heap, createObject("hejsa", i), i);
+    heap_insert(&heap, createObject("Tester", i), i);
   }
   heap_pop(&heap);
   heap_pop(&heap);
-  heap_insert(&heap, createObject("hejsa", 1), 1);
+  heap_insert(&heap, createObject("Tester", 1), 1);
   assertTrue("testHeapSize", heap_size(&heap) == 15);
   heap_clear(&heap);
 }
@@ -75,8 +82,8 @@ void testHeapSize() {
 void testHeapInsert() {
   heap_t heap;
   heap_initialize(&heap);
-  heap_insert(&heap, createObject("hejsa", 1), 1);
-  assertTrue("testHeapInsert", strcmp((char *)heap_pop(&heap), "hejsa: 1") == 0);
+  heap_insert(&heap, createObject("Tester", 1), 1);
+  assertTrue("testHeapInsert", strcmp((char *)heap_pop(&heap), "Tester: 1") == 0);
   heap_clear(&heap);
 }
 
