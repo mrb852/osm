86fe8951dd94e5b87ae88d7956dd4d495b4aed59
Christian Enevoldsen <chrene91@gmail.com>
2015-03-03 14:23:11 +0100

    Adds g4 template code

---
+++ G4/buenos/scripts/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+cd ~/test/shared/G4/buenos/buenos-g4 &&
+make &&
+cd ~/test/shared/G4/buenos/buenos-g4/tests &&
+make &&
+cd .. &&
+util/tfstool delete fyams.harddisk spawner
+util/tfstool delete fyams.harddisk kat
+util/tfstool delete fyams.harddisk mus
+util/tfstool write fyams.harddisk tests/spawner spawner &&
+util/tfstool write fyams.harddisk tests/kat kat &&
+util/tfstool write fyams.harddisk tests/mus mus &&
+fyams-sim buenos initprog=[disk]spawner
